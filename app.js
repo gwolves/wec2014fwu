f0dd51a799e10133aa8776929912e3c9d88ffb16
gwolves <gwolves@snucse.org>
2014-10-10 02:09:38 +0900

    Installed and set up modules

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejsengine = require('ejs-locals');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,6 +13,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('ejs', ejsengine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -21,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session);
 
 app.use('/', routes);
 app.use('/users', users);
@@ -58,3 +62,5 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+app.listen(8724);
