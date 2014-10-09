4594e78b0ff465b42d7fb8fd8087229b96fa5725
gwolves <gwolves@snucse.org>
2014-10-10 03:25:56 +0900

    Add session module

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session);
+app.use(session({
+  secret: 'qwerty',
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.use('/', routes);
 app.use('/users', users);
